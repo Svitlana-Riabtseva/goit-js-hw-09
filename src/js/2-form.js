526e094ffc33b505fedfe0e1c0649c44dacbfa67
Svitlana Riabtseva <161156007+Svitlana-Riabtseva@users.noreply.github.com>
2024-08-08 14:30:07 +0300

    add formData

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,51 +1,50 @@
-const feedBackFormEl = document.querySelector('.feedback-form');
-let formData = {
-  email: '',
-  message: '',
+const feedbackFormEl = document.querySelector('.feedback-form');
+let formData = { email: '', message: '' };
+
+const onFormFieldChange = event => {
+  const fieldValue = event.target.value;
+  const fieldName = event.target.name;
+
+  formData[fieldName] = fieldValue;
+
+  localStorage.setItem('feedback-form', JSON.stringify(formData));
 };
+feedbackFormEl.addEventListener('input', onFormFieldChange);
 
 const fillFormFields = () => {
-  const formDataFromLs = JSON.parse(localStorage.getItem('.feedback-form'));
+  const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form'));
 
-  if (formDataFromLs === null) {
+  if (formDataFromLS === null) {
     return;
   }
-  formData = formDataFromLs;
+  formData = formDataFromLS;
 
-  for (const key in formDataFromLs) {
-    if (formDataFromLs.hasOwnProperty(key)) {
-      feedBackFormEl.elements[key].value = formDataFromLs[key];
+  console.log(formDataFromLS);
+
+  for (const key in formDataFromLS) {
+    if (formDataFromLS.hasOwnProperty(key)) {
+      console.log(formDataFromLS);
+      console.dir(feedbackFormEl.elements[key].value);
+
+      feedbackFormEl.elements[key].value = formDataFromLS[key];
     }
   }
 };
 fillFormFields();
 
-const onFormFieldChange = event => {
-  const fieldName = event.target.name;
-  const fieldValue = event.target.value;
-
-  formData[fieldName] = fieldValue;
-
-  localStorage.setItem('.feedback-form', JSON.stringify(formData));
-};
-
 const onFeedbackFormSubmit = event => {
   event.preventDefault();
 
-  const email = formData.email.trim();
-  const message = formData.message.trim();
+  const email = feedbackFormEl.elements['email'].value.trim();
+  const message = feedbackFormEl.elements['message'].value.trim();
 
-  if (email === '' || message === '') {
+  if (!email || !message) {
     alert('Fill please all fields');
     return;
   }
+
   event.target.reset();
-  localStorage.removeItem('.feedback-form');
-  console.log(formData);
+  localStorage.removeItem('feedback-form');
+  formData = { email: '', message: '' };
 };
-
-localStorage.removeItem('.feedback-form'); // чиститься локал сторідж точечно по ключу
-formData = { email: '', message: '' }; // чиститься обʼект formData
-
-feedBackFormEl.addEventListener('input', onFormFieldChange);
-feedBackFormEl.addEventListener('submit', onFeedbackFormSubmit);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
